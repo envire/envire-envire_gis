f898d7cb8ccc0861086ff5b1b79720c3d4a00e38
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2016-02-08 12:01:50 +0100

    Initial commit

---
+++ src/Dummy.cpp
@@ -0,0 +1,10 @@
+#include "Dummy.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace envire_gis;
+
+void DummyClass::welcome()
+{
+    cout << "You successfully compiled and executed DummyProject. Welcome!" << endl;
+}
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <envire_gis/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    envire_gis::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
